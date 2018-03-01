ad39a9eb90a2bff2f16fbf09de9730e1eab6aa01
bazmatic <barry@earsman.com>
2018-03-01 16:39:48 +1000

    Simple web service and database for oracle.

--- oracle/oracle.js
+++ oracle/oracle.js
@@ -1,17 +1,16 @@
 var fetch = require('node-fetch');
 var Web3 = require('web3');
-var Contract = require('../src/components/Contract');
 var monaLeaseContractBuild = require('../build/MonaLeaseDeployment.json');
 var truffleContract = require('truffle-contract');
 var ExchangeMarketURL = {
-  IndependentReserve: 'https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=eth&secondaryCurrencyCode=aud',
-  BtcMarkets: 'https://api.btcmarkets.net/market/BTC/AUD/tick'
+  IndependentReserve: 'https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=eth&secondaryCurrencyCode=aud'
+  //BtcMarkets: 'https://api.btcmarkets.net/market/BTC/AUD/tick'
 };
 var rates =  {
   AUD: 0
 };
 var cron = require('node-cron');
-var web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
+var web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545')); //Use RPC URL of Geth node or Infura
 
 var MonaLeaseContract = truffleContract({
   abi: monaLeaseContractBuild.abi
@@ -23,24 +22,29 @@ async function MarketExchange(url) {
   try {
     const response = await fetch(url);
     const json = await response.json();
-    return (json.LastPrice || (json.lastPrice)/10);
+    return (json.LastPrice);
   } catch (error) {
     console.log(error);
   }
 }
 //buggy way of finding average -> need to change it
 const ObtainRate = () => {
+  MarketExchange(ExchangeMarketURL.IndependentReserve).then(function (rate) {
+    rates.AUD = rate;
+  })
+  /*
   Object.values(ExchangeMarketURL).map(function(url, index) {
     (MarketExchange(url)).then(function (rate) {
       rates.AUD += rate;
       rates.AUD /= index+1;
     });
-  });
+  });*/
 }
+/*
 cron.schedule('* * * * *', function () {
   ObtainRate();
 });
-ObtainRate();
+ObtainRate();*/
 
 module.exports = {
   ExchangeRate: function () {
--- oracle/server.js
+++ oracle/server.js
@@ -1,12 +1,63 @@
-var http = require('http');
-var oracle = require('./oracle.js');
-
+/*
 function onUserRequest(request, response) {
   console.log("User's address is: " + request.url);
   response.writeHead(200, {"Context-Type": "Text-Type"});
-  response.write("Today's average price of Eth in AUD is " + oracle.ExchangeRate().AUD.toString());
+  response.write("Today's average price of Eth in AUD is " + );
   response.end();
 }
 
 http.createServer(onUserRequest).listen(8888);
-console.log("The server is running");
+*/
+
+'use strict';
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+const oracle = require('./oracle.js'); 
+const db = require('./db.js');
+
+const oracleServer = express();
+oracleServer.use(bodyParser.json());
+oracleServer.use(bodyParser.urlencoded({ extended: true }));
+//oracle.use(allowCrossDomain);
+oracleServer.use(cookieParser());
+
+oracleServer.get('/', function(req, res) {
+	res.status(200).send("MonaLease Oracle");
+});
+
+oracleServer.get('/rate/:code', function(req, res) {
+  res.status(200).json(
+    {
+      "result": oracle.ExchangeRate()[req.params.code].toString()
+    }
+  );
+});
+
+oracleServer.post('/lease', function(req, res) {
+  //Save to database
+  try {
+     db.insert(req.body);
+     res.status(200).json({status: "success", data: req.body});
+  }
+  catch (e) {
+    res.status(500).json({status: "fail", error: e});
+  }
+ 
+  
+});
+
+http.createServer(oracleServer).listen(8888, function(err) {
+	if (err) {
+		console.error(err);
+	}
+	else {
+		console.log("Oracle listening");
+	}
+});
+
+/*
+var DAY = 1000 * 60 * 60 * 24;
+setInterval(function doStuff() {}, DAY)
+*/
